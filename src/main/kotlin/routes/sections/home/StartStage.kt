69df5c7ce8503cb5df6fd5123ef96bbc2ffc62ab
Sudo Dios <sudodios@gmail.com>
2024-10-14 12:12:06 +0330

    Display the current network interface

--- src/main/kotlin/routes/sections/home/StartStage.kt
+++ src/main/kotlin/routes/sections/home/StartStage.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,16 +25,29 @@ import components.AnimatedText
 import components.StartButton
 import core.Service
 import dev.icerock.moko.mvvm.livedata.compose.observeAsState
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 import theme.ColorBox
+import theme.Fonts
+import util.NetUtils
 
 @Composable
 fun StartStage(onStartClicked: () -> Unit, onChooseServerClicked: () -> Unit) {
 
     val testPoint = Service.testPoint.observeAsState()
     var isReady by remember { mutableStateOf(false) }
+    var networkAdapter by remember { mutableStateOf("") }
 
     LaunchedEffect(Unit) {
+        withContext(Dispatchers.IO) {
+            val netInterface = NetUtils.getDefaultNetworkInterface()
+            networkAdapter = if (netInterface != null) {
+                "${netInterface.name} (${NetUtils.parseMacAddress(netInterface.hardwareAddress)})"
+            } else {
+                "Unknown"
+            }
+        }
         delay(2000)
         isReady = true
     }
@@ -91,6 +105,22 @@ fun StartStage(onStartClicked: () -> Unit, onChooseServerClicked: () -> Unit) {
                 tint = ColorBox.text.copy(0.8f)
             )
         }
+
+        Row(modifier = Modifier.padding(top = 20.dp),verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                modifier = Modifier.size(18.dp),
+                painter = painterResource("icons/network.svg"),
+                contentDescription = null,
+                tint = ColorBox.text.copy(0.6f)
+            )
+            Text(
+                modifier = Modifier.padding(start = 16.dp),
+                text = networkAdapter,
+                color = ColorBox.text.copy(0.6f),
+                style = MaterialTheme.typography.bodySmall.copy(fontFamily = Fonts.open_sans)
+            )
+        }
+
     }
 
 }
